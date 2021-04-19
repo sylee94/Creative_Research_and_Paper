b2f9c714a8fd62b99275e329509a03235b46cb7e
sylee94 <81943479+sylee94@users.noreply.github.com>
2021-04-19 10:36:15 +0900

    Add files via upload
    Test Feature Suite

---
+++ TestFeatureSuite.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sangyeoplee;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+/**
+ * Sample JUnit-4-style test suite.
+ * 
+ * @author nb
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sangyeoplee.CaculatorTest.class,sangyeoplee.VectorsTest.class})
+
+public class TestFeatureSuite {
+    
+}
